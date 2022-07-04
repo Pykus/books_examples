8c5ddc40a91584b4b9aa385e868c4be313d134b7
Pykus <przemyslaw.przywarty@outlook.com>
2022-07-04 18:53:06 +0200

    Add Soundex.h, a few TDD steps

---
+++ Soundex.h
@@ -0,0 +1,25 @@
+#ifndef Soundex_h
+#define Soundex_h
+#include <string>
+class Soundex {
+public:
+  std::string encode(const std::string &word) const {
+    auto encoded = word.substr(0, 1);
+    if (word.length() > 1)
+      encoded += "1";
+    return zeroPad(encoded);
+  }
+
+private:
+  std::string head(const std::string &word) const { return word.substr(0, 1); };
+  std::string encodedDigits(const std::string &word) const {
+    if (word.length() > 1)
+      return "1";
+    return "";
+  };
+  std::string zeroPad(const std::string &word) const {
+    auto zerosNeeded = 4 - word.length();
+    return word + std::string(zerosNeeded, '0');
+  };
+};
+#endif
--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -1,17 +1,19 @@
-cd#include <gtest/gtest.h> 
+#include "Soundex.h"
+#include <gtest/gtest.h>
 
-class Soundex
-{
+// using namespace testing;
+class SoundexEncoding : public testing::Test {
 public:
-    std::string encode(const std::string &word) const
-    {
-        return "A";
-    }
+  Soundex soundex;
 };
-
-TEST(SoundexEncoding, RetainSoleLetterOfOneLetterWord)
-{
-    Soundex soundex;
-    auto encoded = soundex.encode("A");
-    ASSERT_EQ(encoded, "A");
+TEST_F(SoundexEncoding, RetainSoleLetterOfOneLetterWord) {
+  auto encoded = soundex.encode("A");
+  ASSERT_EQ(encoded, "A000");
+}
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+  auto encoded = soundex.encode("I");
+  ASSERT_EQ(encoded, "I000");
+}
+TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits) {
+  ASSERT_EQ(soundex.encode("Ab"), "A100");
 }
